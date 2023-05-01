e3bf28ee9ae118b21de5ede28b60d99c2ac4e6f8
dede_dede_ahmet@hotmail.com <dede_dede_ahmet@hotmail.com>
2023-05-01 12:14:03 +0300

    #sys2 ESLint configuration set

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,15 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
+    'prettier'
   ],
   rules: {
     'vue/multi-word-component-names': 'off',
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'prefer-const': ['error', {
+      destructuring: 'all',
+      ignoreReadBeforeAssign: true,
+    }]
   },
 }
