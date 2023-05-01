5f01c287077fb6cbbd17109b94bfbad87c3160f6
dede_dede_ahmet@hotmail.com <dede_dede_ahmet@hotmail.com>
2023-05-01 12:41:42 +0300

    #sys3 Deleted default settings in code base and made ready to fresh coding

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,17 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
 
+import HomePage from '@/components/Home.vue'
+
 const routes = [
   {
     path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    component: () => import('@/views/HomeBase.vue'),
     children: [
       {
         path: '',
         name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        component: HomePage,
       },
     ],
   },
---
+++ src/views/HomeBase.vue
@@ -0,0 +1,7 @@
+<template>
+  <home/>
+</template>
+
+<script lang="ts" setup>
+  import Home from '@/components/Home.vue'
+</script>
