169b3dcd42e2642953dabf73598b6257bdcc12b8
Ahmet Dede <dede_dede_ahmet@hotmail.com>
2023-06-01 21:02:33 +0300

    inline style removed

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,22 +1,23 @@
 <template>
   <v-navigation-drawer
-          color="primary"
-          expand-on-hover
-          rail
-          elevation="0"
-          rounded="0"
-        >
-          <v-list>
-            <v-list-item
-              prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-              title="Sandra Adams"
-              subtitle="sandra_a88@gmailcom"
-            />
-          </v-list>
-          <v-list density="compact" nav>
-            <v-list-item prepend-icon="mdi-folder"></v-list-item>
-          </v-list>
-        </v-navigation-drawer>
+    color="primary"
+    expand-on-hover
+    rail
+    elevation="0"
+    rounded="0"
+  >
+    <v-list>
+      <v-list-item
+        prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+        title="Şamil Şıracı"
+        subtitle="Prime Minister"
+      />
+    </v-list>
+    <v-divider/>
+    <v-list density="compact" nav>
+      <v-list-item prepend-icon="mdi-folder"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script setup lang="ts">
