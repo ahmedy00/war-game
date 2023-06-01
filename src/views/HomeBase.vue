6147443341eaa77ca93cec42499b8adbfc9b560f
Ahmet Dede <dede_dede_ahmet@hotmail.com>
2023-06-01 21:54:44 +0300

    Deleted unused variable

--- src/views/HomeBase.vue
+++ src/views/HomeBase.vue
@@ -5,7 +5,7 @@
         <NavBar/>
         <v-main class="main d-flex flex-row">
           <MapBase/>
-          <CountryInformation :is-card-visible="isCardVisible"/>
+          <CountryInformation/>
         </v-main>
       </v-layout>
     </v-card>
@@ -13,12 +13,9 @@
 </template>
 
 <script setup lang="ts">
-import { Ref, ref } from 'vue'
 import MapBase from '@/components/MapBase.vue'
 import NavBar from '@/components/NavBar.vue'
 import CountryInformation from '@/components/CountryInformation.vue'
-
-const isCardVisible: Ref<boolean> = ref(false)
 
 </script>
 
