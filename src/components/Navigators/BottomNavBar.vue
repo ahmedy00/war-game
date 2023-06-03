b1713cc73930c56925f58dabd5dd0bd9fc809a02
Ahmet Dede <dede_dede_ahmet@hotmail.com>
2023-06-04 01:05:31 +0300

    Set theme change feature

--- src/components/Navigators/BottomNavBar.vue
+++ src/components/Navigators/BottomNavBar.vue
@@ -13,17 +13,43 @@
       Nearby
     </v-btn>
     <v-spacer/>
-    <v-btn>
-      <v-icon>mdi-theme-light-dark</v-icon>
-      Change Theme
-    </v-btn>
+    <!-- TODO: Find a better way to make cursor pointer -->
+    <v-menu class="cursor-pointer" :location="'top'">
+      <template v-slot:activator="{ props }">
+        <v-btn v-bind="props">
+          <v-icon>mdi-theme-light-dark</v-icon>
+            Change Theme
+        </v-btn>
+      </template>
+        <v-list dense>
+          <v-list-item v-for="(themeName, index) in themeNames" :key="index">
+            <v-list-item-title @click="changeTheme(themeName)">
+              {{ themeName }}
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+    </v-menu>
 </v-bottom-navigation>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useAppStore } from '@/store/app'
+
+
+const appStore = useAppStore()
+
+const themeNames = ref(['Light', 'Dark'])
+
+const changeTheme = (themeName: string) => {
+  appStore.changeTheme(themeName)
+}
 
 </script>
 
 <style scoped>
+  .cursor-pointer {
+    cursor: pointer;
+  }
 
 </style>
