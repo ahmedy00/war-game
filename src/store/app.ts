89c59f8eb0943388f6b028998cdc7daa1cfab972
Ahmet Dede <dede_dede_ahmet@hotmail.com>
2023-06-04 00:37:51 +0300

    state and actions set for theme change

--- src/store/app.ts
+++ src/store/app.ts
@@ -3,6 +3,11 @@ import { defineStore } from 'pinia'
 
 export const useAppStore = defineStore('app', {
   state: () => ({
-    //
+    selectedTheme: ''
   }),
+  actions: {
+    changeTheme (theme: string) {
+      this.selectedTheme = theme
+    }
+  }
 })
