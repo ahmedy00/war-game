5f52897e02b9ec92a22c23521bcead4f96bfa69d
Ahmet Dede <dede_dede_ahmet@hotmail.com>
2023-06-04 00:53:47 +0300

    Added ability to change theme main color to changeTheme function

--- src/store/app.ts
+++ src/store/app.ts
@@ -3,11 +3,13 @@ import { defineStore } from 'pinia'
 
 export const useAppStore = defineStore('app', {
   state: () => ({
-    selectedTheme: ''
+    selectedTheme: '',
+    themeMainColor: ''
   }),
   actions: {
     changeTheme (theme: string) {
       this.selectedTheme = theme
+      this.themeMainColor = theme === 'dark' ? '#2c302d' : '#d3ded6'
     }
   }
 })
