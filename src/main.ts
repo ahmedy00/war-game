cf2832ed7a542f8d0f2ea24d8bcf3a1197d274c0
Ahmet Dede <dede_dede_ahmet@hotmail.com>
2023-06-08 22:57:34 +0300

    Moved vue-echarts import to appropriate location

--- src/main.ts
+++ src/main.ts
@@ -13,12 +13,9 @@ import { createApp } from 'vue'
 // Plugins
 import { registerPlugins } from '@/plugins'
 
-// Vue Echarts
-import VueEcharts from 'vue-echarts'
 
 const app = createApp(App)
 
 registerPlugins(app)
 
-app.component('v-chart', VueEcharts)
 app.mount('#app')
