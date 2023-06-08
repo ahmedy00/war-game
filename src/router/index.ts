1c8ca044bc0e3e25038f7bffaa24a10a55ef1c72
Ahmet Dede <dede_dede_ahmet@hotmail.com>
2023-06-08 22:59:12 +0300

    Deleted unnecessary route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,10 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
 
-import MapBase from '@/components/MapBase.vue'
-
 const routes = [
   {
     path: '/',
     component: () => import('@/views/HomeBase.vue'),
-    children: [
-      {
-        path: '',
-        name: 'MapBase',
-        component: MapBase,
-      },
-    ],
   },
 ]
 
