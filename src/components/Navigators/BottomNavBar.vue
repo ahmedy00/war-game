7fb395d1217acf73040fac99f260574653e602c7
Ahmet Dede <dede_dede_ahmet@hotmail.com>
2023-06-08 23:12:32 +0300

    Colors are adjusted for light and dark theme in only bottom nav bar

--- src/components/Navigators/BottomNavBar.vue
+++ src/components/Navigators/BottomNavBar.vue
@@ -1,24 +1,34 @@
 <template>
-  <v-bottom-navigation :style="{background: appStore.theme.mainColor}" grow>
-    <v-btn :color="appStore.theme.oppositeColor" value="recent">
-      <v-icon>mdi-history</v-icon>
-      Recent
+  <v-bottom-navigation
+    :style="{background: appStore.theme.mainColor}"
+    grow>
+    <v-btn
+      :color="appStore.theme.oppositeColor"
+      value="recent">
+      <v-icon :color="appStore.theme.oppositeColor">mdi-history</v-icon>
+      <span :style="{color: appStore.theme.oppositeColor}">Recent</span>
     </v-btn>
-    <v-btn :color="appStore.theme.oppositeColor" value="favorites">
-      <v-icon>mdi-heart</v-icon>
-      Favorites
+    <v-btn
+      :color="appStore.theme.oppositeColor"
+      value="favorites">
+      <v-icon :color="appStore.theme.oppositeColor">mdi-heart</v-icon>
+      <span :style="{color: appStore.theme.oppositeColor}">Favorites</span>
     </v-btn>
-    <v-btn :color="appStore.theme.oppositeColor" value="nearby">
-      <v-icon>mdi-map-marker</v-icon>
-      Nearby
+    <v-btn
+      :color="appStore.theme.oppositeColor"
+      value="nearby">
+      <v-icon :color="appStore.theme.oppositeColor">mdi-map-marker</v-icon>
+      <span :style="{color: appStore.theme.oppositeColor}">Nearby</span>
     </v-btn>
     <v-spacer/>
     <!-- TODO: Find a better way to make cursor pointer -->
     <v-menu class="cursor-pointer" :location="'top'">
       <template v-slot:activator="{ props }">
-        <v-btn :color="appStore.theme.oppositeColor" v-bind="props">
-          <v-icon>mdi-theme-light-dark</v-icon>
-            Change Theme: {{ appStore.theme.name }}
+        <v-btn
+          :color="appStore.theme.oppositeColor"
+          v-bind="props">
+          <v-icon :color="appStore.theme.oppositeColor">mdi-theme-light-dark</v-icon>
+            <span :style="{color: appStore.theme.oppositeColor}">Change Theme: {{ appStore.theme.name }}</span>
         </v-btn>
       </template>
         <v-list dense>
@@ -38,6 +48,7 @@ import { useAppStore } from '@/store/app'
 
 
 const appStore = useAppStore()
+console.log('color', appStore.theme)
 
 const themeNames = ref(['Light', 'Dark'])
 
